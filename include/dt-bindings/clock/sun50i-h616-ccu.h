6dbeac14f696e052d8a22567159f159a1c7efad7
Ryan Walklin <ryan@testtoast.com>
2024-05-17 07:39:09 +1200

    Add TCON clock/reset

--- include/dt-bindings/clock/sun50i-h616-ccu.h
+++ include/dt-bindings/clock/sun50i-h616-ccu.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/D* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
 /*
  * Copyright (C) 2020 Arm Ltd.
  */
@@ -113,5 +113,7 @@
 #define CLK_BUS_HDCP		127
 #define CLK_PLL_SYSTEM_32K	128
 #define CLK_BUS_GPADC		129
+#define CLK_TCON_LCD0   130
+#define CLK_BUS_TCON_LCD0 131
 
 #endif /* _DT_BINDINGS_CLK_SUN50I_H616_H_ */
